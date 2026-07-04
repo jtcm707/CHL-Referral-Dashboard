8fd0f68b5293168ab382446f4b8d3b9890c33bea
jtcm707 <jtcm707@gmail.com>
2026-07-04 10:09:16 -0400

    Update contact.js

--- contact.js
+++ contact.js
@@ -17,6 +17,6 @@
    project reference.
    ========================================================================= */
 window.CHT_CONFIG = {
-  SUPABASE_URL: 'https://rbtdoikfaouxhleimakd.supabase.co',
-  SUPABASE_ANON_KEY: 'sb_publishable_GOjkF69Y3R4TPLL1JtdwZA_qaCrcS1u',
+  SUPABASE_URL: 'https://eeejjoxwheydrrwdasph.supabase.co',
+  SUPABASE_ANON_KEY: 'sb_publishable_lYqknXA6GFgMEZWRGEinkA_SEI1norq',
 };
